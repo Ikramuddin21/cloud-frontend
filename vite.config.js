7a4bcb115d8c8d35232e50fa4218fa6bdbc79896
ikramuddin21 <shaonakhand2122@gmail.com>
2024-12-26 10:25:16 +0600

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 // vite.config.js
-// const { resolve } = require("path");
+const { resolve } = require("path");
 import tailwindcss from "tailwindcss";
 
 module.exports = {
   build: {
     rollupOptions: {
       input: {
-        index: "index.html",
-        view: "view.html",
+        index: resolve("index.html"),
+        view: resolve("view.html"),
       },
     },
   },
